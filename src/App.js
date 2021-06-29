adcb339a8a9fc507d1f7e674252c222c77c295c7
h-cue <hmabrantes@hotmail.com>
2021-06-30 01:36:14 +0200

    e

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     const [load, upadateLoad] = useState(true);
     useEffect(() => {
         setTimeout(() => {
-            upadateLoad(false);
+            upadateLoad(true);
         }, 1200);
     }, []);
 
